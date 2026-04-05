e3994ca580a112c2f12a9697a3c0028581ba09f9
restyle5 <abdurtinker@gmail.com>
2026-04-05 18:58:43 +0800

    drop inventory module

--- src/main/java/com/example/inventory/repository/InventoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.inventory.repository;
-
-import com.example.inventory.model.Inventory;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InventoryRepository extends MongoRepository<Inventory, String> {
-}
